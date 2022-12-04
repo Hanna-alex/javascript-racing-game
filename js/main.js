8ee0e263d9459569fb84087032d9b14bd6e5f082
Hanna-alex <64468077+Hanna-alex@users.noreply.github.com>
2022-12-04 21:05:09 +0600

    fixed visibility cars

--- js/main.js
+++ js/main.js
@@ -59,7 +59,7 @@ function startGame() {
     enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
     enemy.style.top = enemy.y + 'px';
     enemy.style.background = `transparent 
-    url(../img/enemy${getRandomEnemy(MAX_ENEMY)}.png)
+    url(./img/enemy${getRandomEnemy(MAX_ENEMY)}.png)
     center / cover
     no-repeat`;
     gameArea.appendChild(enemy);
